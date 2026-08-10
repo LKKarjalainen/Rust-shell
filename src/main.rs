8c0892688a1c7e08f5b616f93070028c57a6b369
LKKarjalainen <lassikarjalainen0@gmail.com>
2026-08-10 21:25:46 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::process::Command;
+
+const BUILT_IN: &[&str] = &["echo", "type", "exit"];
 
 fn main() {
     loop {
@@ -13,6 +14,18 @@ fn main() {
         let command: &str = input_vec[0];
         //println!("command: {:?}", input_vec);
         //println!("args: {:?}", args);
+        if command.trim() == "type" {
+            let mut output: String = String::new();
+            output.push_str(args[0].trim());
+            if BUILT_IN.contains(&args[0].trim()) {
+                output.push_str(" is a shell builtin");
+            }
+            else {
+                output.push_str(": not found");
+            }
+            println!("{}", output);
+            continue;
+        }
         if command.trim() == "echo" {
             let mut output: String = String::new();
             for arg in args {
