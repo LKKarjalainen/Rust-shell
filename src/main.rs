842a3d31e34aa03440bfd4b56097d4150af95afa
LKKarjalainen <lassikarjalainen0@gmail.com>
2026-08-10 19:29:21 +0300

    start

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use std::io::Read;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
+    let mut input:String = String::new();
     io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input)?;
+    println!("{}", input);
 }
