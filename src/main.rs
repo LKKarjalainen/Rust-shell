21fda944c9988fd2cb10a503a1ec1cde39e91b41
LKKarjalainen <lassikarjalainen0@gmail.com>
2026-08-10 20:59:18 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::Command;
 
 fn main() {
     loop {
@@ -7,9 +8,23 @@ fn main() {
         let mut input:String = String::new();
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut input).unwrap();
-        if input.trim() == "exit" {
+        let mut input_vec:Vec<&str> = input.split(' ').collect();
+        let args: Vec<&str> = input_vec.split_off(1);
+        let command: &str = input_vec[0];
+        //println!("command: {:?}", input_vec);
+        //println!("args: {:?}", args);
+        if command.trim() == "echo" {
+            let mut output: String = String::new();
+            for arg in args {
+                output.push_str(arg);
+                output.push(' ');
+            }
+            print!("{}", output);
+            continue;
+        }
+        if command.trim() == "exit" {
             break;
         }
-        println!("{}: command not found", input.trim());
+        println!("{}: command not found", command.trim());
     }
 }
