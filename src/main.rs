86d5d67c046958d4d203bf23f486829513433cf4
LKKarjalainen <lassikarjalainen0@gmail.com>
2026-08-10 19:44:04 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ fn main() {
         let mut input:String = String::new();
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut input).unwrap();
+        if input == "exit" {
+            break;
+        }
         println!("{}: command not found", input.trim());
     }
 }
